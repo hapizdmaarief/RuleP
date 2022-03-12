436926a7da228a521b4d99bb6e71e6f07c3069d5
hapizdmaarief <53427704+hapizdmaarief@users.noreply.github.com>
2022-03-13 03:13:40 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,237 @@
+#! /usr/bin/bash
+null="> /dev/null 2>&1"
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+mkdir -p $HOME/.config/clash
+echo ""
+
+echo -e $b"➤ "$w"Clash For Termux Non Root"
+echo ""
+
+echo -e $b"➤ "$w" prepare for installing dependencies ..."
+echo ""
+
+cd $HOME/RuleP
+sleep 2
+
+echo -e $b"➤ "$w" coppying LAN.yaml: "$g"LAN"$w
+cp -r -f LAN.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying applemaster.yaml: "$g"apple"$w
+cp -r -f applemaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying browsermaster.yaml: "$g"browser"$w
+cp -r -f browsermaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying callofdutymaster.yaml: "$g"callofduty"$w
+cp -r -f callofdutymaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying deezermaster.yaml: "$g"dezzer"$w
+cp -r -f deezermaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying disneymaster.yaml: "$g"disney"$w
+cp -r -f disneymaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying facebookmaster.yaml: "$g"facebook"$w
+cp -r -f facebookmaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying garenamaster.yaml: "$g"garena"$w
+cp -r -f garenamaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying iklanmaster.yaml: "$g"iklan"$w
+cp -r -f iklanmaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying instagrammaster.yaml: "$g"instagram"$w
+cp -r -f instagrammaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying langitmusikmaster.yaml: "$g"langitmusik"$w
+cp -r -f langitmusikmaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying maxtreammaster.yaml: "$g"maxtream"$w
+cp -r -f maxtreammaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying mlmaster.yaml: "$g"ml"$w
+cp -r -f mlmaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying molamaster.yaml: "$g"mola"$w
+cp -r -f molamaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying netflixmaster.yaml: "$g"netflix"$w
+cp -r -f netflixmaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying newcfamaster.yaml: "$g"newcfa"$w
+cp -r -f newcfamaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying nimomaster.yaml: "$g"nimo"$w
+cp -r -f nimomaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying onlineshopmaster.yaml: "$g"onlineshop"$w
+cp -r -f onlineshopmaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying pubgmaster.yaml: "$g"pubg"$w
+cp -r -f pubgmaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying rctiplusmaster.yaml: "$g"rctiplus"$w
+cp -r -f rctiplusmaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying ressomaster.yaml: "$g"resso"$w
+cp -r -f ressomaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying restreammaster.yaml: "$g"restream"$w
+cp -r -f restreammaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying robloxmaster.yaml: "$g"roblox"$w
+cp -r -f robloxmaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying smulemaster.yaml: "$g"smule"$w
+cp -r -f smulemaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying soundcloudmaster.yaml: "$g"soundcloud"$w
+cp -r -f soundcloudmaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying speedtestmaster.yaml: "$g"speedtest"$w
+cp -r -f speedtestmaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying spotifymaster.yaml: "$g"spotify"$w
+cp -r -f spotifymaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying starmakermaster.yaml: "$g"starmaker"$w
+cp -r -f starmakermaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying tiktokmaster.yaml: "$g"tiktok"$w
+cp -r -f tiktokmaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying twittermaster.yaml: "$g"twitter"$w
+cp -r -f twittermaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying universitasmaster.yaml: "$g"universitas"$w
+cp -r -f universitasmaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying vidiomaster.yaml: "$g"vidio"$w
+cp -r -f vidiomaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying wesingmaster.yaml: "$g"wesing"$w
+cp -r -f wesingmaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying whatsappmaster.yaml: "$g"whatsapp"$w
+cp -r -f whatsappmaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying youtubemaster.yaml: "$g"youtube"$w
+cp -r -f youtubemaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying zoommaster.yaml: "$g"zoom"$w
+cp -r -f zoommaster.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+cd
+rm -r -f RuleP
+clear
